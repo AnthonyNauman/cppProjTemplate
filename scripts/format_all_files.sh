c82b603d087ed0bb733039efb03b472aa1942fa7
nao <naumenko.ao@mail.ru>
2024-12-20 08:26:48 +0300

    [Refactoring] FIX: move all scripts to folder; change tasks

---
+++ scripts/format_all_files.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+source $(dirname $0)/project_info.sh
+
+find "$SOURCE_DIR" -type f \( -name "*.c" -o -name "*.cpp" -o -name "*.h" -o -name "*.hpp" \) | while read -r file; do
+    clang-format -i "$file"
+    echo "Formatted: $file"
+done
--- scripts/project_info.sh
+++ scripts/project_info.sh
@@ -2,27 +2,25 @@
 
 # === Project variables ===
 
-PROJ_NAME="App"
-PROJ_VERSION_MAJOR=1
-PROJ_VERSION_MINOR=0
+PROJ_NAME="app"
+PROJ_VERSION_MAJOR=4
+PROJ_VERSION_MINOR=2
 PROJ_VERSION_BUILD=0
-PROJ_VERSION_PATCH=0
+PROJ_VERSION_PATCH=7
 PROJ_VERSION="$PROJ_VERSION_MAJOR.$PROJ_VERSION_MINOR.$PROJ_VERSION_BUILD.$PROJ_VERSION_PATCH"
 
 # === Project variables ===
-
-
-
 
 
 # ================================
 SCRIPTS_DIR=$( dirname -- "$( readlink -f -- "$0"; )"; )
 PROJ_DIR="$(dirname "$SCRIPTS_DIR")"
 SOURCE_DIR=$PROJ_DIR/apps
+BUILD_DIR=$PROJ_DIR/build
 DEV_OPS_DIR=$PROJ_DIR/devops
 
-echo "=============="
-echo "PROJ DIR: $PROJ_DIR"
-echo "SOURCE DIR: $SOURCE_DIR"
-echo "PROJ Name: $PROJ_NAME"
-echo "PROJ Ver: $PROJ_VERSION"
+# echo "=============="
+# echo "PROJ DIR: $PROJ_DIR"
+# echo "SOURCE DIR: $SOURCE_DIR"
+# echo "PROJ Name: $PROJ_NAME"
+# echo "PROJ Ver: $PROJ_VERSION"
