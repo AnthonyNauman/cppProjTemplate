9afc86e3bbaf5cd7cfc054fed7f13af843316986
nao <naumenko.ao@mail.ru>
2024-12-22 11:21:48 +0300

    [TEMPLPROJ-2-7] FIX: build.yml

--- scripts/run_all_tests.sh
+++ scripts/run_all_tests.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 source $(dirname $0)/project_info.sh
 
-# Чтение файла построчно
-while IFS= read -r line; do
-	if [[ "$line" == *"tests"* ]]; then
-        $SCRIPTS_DIR/run.sh "$line"
-    fi
-done < "$UTILS_DIR/run/options"
+
+# Папка, которую нужно просмотреть
+directory="$INSTALL_DIR/tests"
+
+# Рекурсивный поиск исполняемых файлов и их запуск
+find "$directory" -type f -exec chmod +x {} \; -exec {} \;
