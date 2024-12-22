a3c5492c9122b80656fc0f47589c91b2564c9778
nao <naumenko.ao@mail.ru>
2024-12-22 11:31:58 +0300

    [TEMPLPROJ-2-7] FIX: build.yml

--- scripts/run_all_tests.sh
+++ scripts/run_all_tests.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-source $(dirname $0)/project_info.sh
-
 
 # Папка, которую нужно просмотреть
-directory="$INSTALL_DIR/tests"
+directory="./../tests/"
 
 # Рекурсивный поиск исполняемых файлов и их запуск
 find "$directory" -type f -exec chmod +x {} \; -exec {} \;
