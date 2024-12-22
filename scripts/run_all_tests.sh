b9d044f1ae175c7fa214311b9ebde86c18429d24
nao <naumenko.ao@mail.ru>
2024-12-22 12:33:30 +0300

    [TEMPLPROJ-2-7] FIX: build.yml

--- scripts/run_all_tests.sh
+++ scripts/run_all_tests.sh
@@ -5,5 +5,8 @@ source $(dirname $0)/project_info.sh
 # Папка, которую нужно просмотреть
 directory="$INSTALL_DIR/tests"
 
+report_folder="$UTILS_DIR/test_results"
+mkdir -p $report_folder
+
 # Рекурсивный поиск исполняемых файлов и их запуск
-find "$directory" -type f -exec chmod +x {} \; -exec {} \;
+find "$directory" -type f -exec chmod +x {} \; -exec {} --gtest_output=xml:$report_folder/xunit.xml \;
