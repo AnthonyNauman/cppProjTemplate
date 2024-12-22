53f48c77f7153c4db587f9037aef21bfef37e4e2
nao <naumenko.ao@mail.ru>
2024-12-22 06:53:31 +0000

    [Refactoring] FIX: change test

--- tests/app/someClassTester/src/main.cpp
+++ tests/app/someClassTester/src/main.cpp
@@ -3,20 +3,20 @@
 
 TEST(Test, unitTestName1)
 {
-    ASSERT_TRUE(3 == 3);
-    ASSERT_TRUE(1 == 1);
+    SomeClass sc;
+    ASSERT_TRUE(3 == sc.sum(3, 0));
+    ASSERT_TRUE(1 == sc.sum(2, -1));
 }
 
 TEST(Test, unitTestName2)
 {
-    ASSERT_TRUE(3 * 4 == (3 * 2 * 2));
-    ASSERT_FALSE(3 * 5 == 3);
+    SomeClass sc;
+    ASSERT_TRUE(12 == (sc.sum(3, 9)));
+    ASSERT_FALSE(15 == sc.sum(3, 5));
 }
 
 int main(int argc, char** argv)
 {
-    SomeClass sc;
-    auto      res = sc.sum(5, 4);
     testing::InitGoogleTest(&argc, argv);
     const auto status = RUN_ALL_TESTS();
 
