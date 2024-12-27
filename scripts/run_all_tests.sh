5d4f1beb2779d14c16e5033420176a6af9a3733c
nao <naumenko.ao@mail.ru>
2024-12-27 04:02:11 +0000

    [Refactoring] FIX: change tests run srcipt

--- scripts/run_all_tests.sh
+++ scripts/run_all_tests.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
-source $(dirname $0)/project_info.sh
 
-cov_folder=$ARTIFACTS_DIR/coverage
-report_folder=$ARTIFACTS_DIR/test_report
-cov_info_folder=$report_folder/coverage_info
+run_all_tests() {
+	artifacts_dir=$1
 
-rm -rf $report_folder
-rm -rf $cov_info_folder
-mkdir -p $report_folder $cov_info_folder
 
-main_cov_info_file=$report_folder/main_coverage.info
+	cov_folder=$artifacts_dir/coverage
+	report_folder=$artifacts_dir/test_report
+	cov_info_folder=$report_folder/coverage_info
 
-directory="$INSTALL_DIR/tests"
-run_tests_with_report "$directory"
+	rm -rf $report_folder
+	rm -rf $cov_info_folder
+	mkdir -p $report_folder $cov_info_folder
 
-# Test coverage
-lcov -c -d $cov_folder -o $main_cov_info_file
-genhtml $main_cov_info_file --output-directory $cov_info_folder
+	main_cov_info_file=$report_folder/main_coverage.info
+
+	directory="$artifacts_dir/bin/tests"
+	run_tests_with_report "$directory"
+
+	# Test coverage
+	lcov -c -d $cov_folder -o $main_cov_info_file
+	genhtml $main_cov_info_file --output-directory $cov_info_folder
+}
+
+run_all_tests $1
+
